5872700de06c58510156eddd7a3515f05b173abf
Luis Loureiro <32186@alunos.isel.pt>
2012-04-16 17:40:05 +0100

    Terminated tutorial 'Using the Users Service'.

--- src/example/project/GuestbookServlet.java
+++ src/example/project/GuestbookServlet.java
@@ -2,10 +2,22 @@ package example.project;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 @SuppressWarnings("serial")
 public class GuestbookServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+
+		if (user != null) {
+			resp.setContentType("text/html");
+			resp.getWriter().println("Hello, " + user.getNickname());
+			resp.getWriter().println("<a href=\""+resp.encodeURL(userService.createLogoutURL("/"))+"\">Sign Out</a>");
+		} else {
+			resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
+		}
 	}
 }
